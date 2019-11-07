50d7884219c06c367f9e85031b3021ddfb249fd6
IgorOl <igor.ol@me.com>
2019-11-07 11:38:01 +0300

    SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,3 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,16 +7,13 @@ let package = Package(
     .macOS(.v10_15), .iOS(.v12), .tvOS(.v12)
   ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Zip",
             targets: ["Zip"]),
-    ]
+    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Zip",
-            path: "Zip"),
+            path: "Zip")
     ]
 )
